db228bfe710f95199c4ab6fc4cbbbde002a69b31
Suncia-J <1852118251@qq.com>
2024-11-26 19:18:21 +0800

    Site updated: 2024-11-26 19:18:21

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1732613320593";
+const VERSION = "1732619898158";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",

6838ae0aaf5bacb5f32e183dcae873589300a7b3
Suncia-J <1852118251@qq.com>
2024-12-09 12:22:27 +0800

    Site updated: 2024-12-09 12:22:26

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1733717796628";
+const VERSION = "1733718140196";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
